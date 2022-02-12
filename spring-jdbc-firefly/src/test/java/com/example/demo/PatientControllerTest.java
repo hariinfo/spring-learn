9caf2e8fd837cd43d525d71b3039090d3658ed04
hariinfo <hari.info@gmail.com>
2022-02-12 12:17:09 -0600

    added junit

--- spring-jdbc-firefly/src/test/java/com/example/demo/PatientControllerTest.java
+++ spring-jdbc-firefly/src/test/java/com/example/demo/PatientControllerTest.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+
 
 @SpringBootTest (classes = DemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
@@ -16,9 +18,9 @@ public class PatientControllerTest {
 
     @Test
     public void getPatients() throws Exception {
+        String actual = "[{\"patient_id\":0,\"firstName\":\"test\",\"lastName\":\"test\"},{\"patient_id\":1,\"firstName\":\"test\",\"lastName\":\"test\"}]";
         ResponseEntity<String> response = testRestTemplate.getForEntity("/patient", String.class);
-        //log.info(response.toString());
-        Assertions.assertThat(!response.getBody().isEmpty());
+        JSONAssert.assertEquals(response.getBody(), actual, JSONCompareMode.LENIENT);
     }
 
 }
